42b938de42f1903f64d73533ad5a596e1eeaa266
Mohammad Ali Zahmatkesh <maz.stick1383@gmail.com>
2023-12-16 00:22:02 +0330

    Done

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -20,15 +21,32 @@ public class Main {
     }
 
     private static String selectProgram() throws IOException {
-
-        RandomAccessFile rfile23 = new RandomAccessFile(".\\test\\Add 100 Numbers Program.txt", "rw");
-        RandomAccessFile rfil = new RandomAccessFile(".\\test\\Subtraction Program.txt", "rw");
-        RandomAccessFile rfile = new RandomAccessFile(".\\test\\Mul Program.txt", "rw");
-        RandomAccessFile rfile4 = new RandomAccessFile(".\\test\\Add Program.txt", "r");
+        String path = pathBuilder();
+        RandomAccessFile rfile = new RandomAccessFile(path, "r");
         StringBuilder result = new StringBuilder();
-        while (rfile.getFilePointer()<rfile.length()){
+        while (rfile.getFilePointer() < rfile.length()) {
             result.append(rfile.readLine()).append("\n");
         }
         return result.toString();
+    }
+
+    private static String pathBuilder() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+        RandomAccessFile menu = new RandomAccessFile(".\\test\\menu.txt", "rw");
+        while (menu.getFilePointer() < menu.length()) {
+            result.append(menu.readLine()).append("\n");
+        }
+        System.out.println(result);
+        int command = scanner.nextInt();
+        menu.seek(0);
+        for (int i = 0; i < command-1; i++) {
+            menu.readLine();
+        }
+        menu.seek(menu.getFilePointer()+2);
+        StringBuilder path = new StringBuilder(menu.readLine());
+        path.insert(0,".\\test\\");
+        path.append(".txt");
+        return path.toString();
     }
 }
