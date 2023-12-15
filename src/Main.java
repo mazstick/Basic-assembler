59d5bf047f999ea7d7db95cb23bd5fdf83b0a385
Mohammad Ali Zahmatkesh <maz.stick1383@gmail.com>
2023-12-15 23:18:59 +0330

    add program file
    just for test

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String assembleCode = selectProgram();
+        System.out.println(assembleCode);
         Library.loadLibrary();
-        String assembleCode = "ORG 1050  \n" +
-                "LDA A \n" +
-                "ADD A I \n" +
-                "STA C \n" +
-                "HLT \n" +
-                "A, DEC 83 \n" +
-                "B, DEC -23 \n" +
-                "C, DEC 10 \n" +
-                "END";
-
-
         Parser parser = new Parser();
         Assembler assembler = new Assembler();
         List<Instruction> instructions = parser.tokenizer(assembleCode);
-        if (instructions == null){
+        if (instructions == null) {
             return;
         }
         assembler.assemble(instructions);
+    }
+
+    private static String selectProgram() throws IOException {
+
+        RandomAccessFile rfile23 = new RandomAccessFile(".\\test\\Add 100 Numbers Program.txt", "rw");
+        RandomAccessFile rfil = new RandomAccessFile(".\\test\\Subtraction Program.txt", "rw");
+        RandomAccessFile rfile = new RandomAccessFile(".\\test\\Mul Program.txt", "rw");
+        RandomAccessFile rfile4 = new RandomAccessFile(".\\test\\Add Program.txt", "r");
+        StringBuilder result = new StringBuilder();
+        while (rfile.getFilePointer()<rfile.length()){
+            result.append(rfile.readLine()).append("\n");
+        }
+        return result.toString();
     }
 }
