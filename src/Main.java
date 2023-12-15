e95e90843246e8ce6d6724296bf9e66274333d07
Mohammad Ali Zahmatkesh <maz.stick1383@gmail.com>
2023-12-16 01:11:41 +0330

    Add some options
    option : user can write program manually

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -39,14 +36,47 @@ public class Main {
         }
         System.out.println(result);
         int command = scanner.nextInt();
+        if (command == 0) {
+            writeFile(menu);
+            return pathBuilder();
+        }
         menu.seek(0);
-        for (int i = 0; i < command-1; i++) {
+        for (int i = 0; i < command; i++) {
             menu.readLine();
         }
-        menu.seek(menu.getFilePointer()+2);
+        menu.seek(menu.getFilePointer() + 2);
         StringBuilder path = new StringBuilder(menu.readLine());
-        path.insert(0,".\\test\\");
+        path.insert(0, ".\\test\\");
         path.append(".txt");
         return path.toString();
+    }
+
+    private static void writeFile(RandomAccessFile menu) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your program name :");
+        String name = scanner.nextLine();
+        System.out.println("Write your program :");
+        String code = "";
+        String str = "";
+        while (scanner.hasNextLine()) {
+            str = scanner.nextLine();
+            code += str + "\n";
+            if (str.equals("END")) break;
+        }
+        String path = ".\\test\\" + name + ".txt";
+        RandomAccessFile rfile = new RandomAccessFile(path, "rw");
+        rfile.write(code.getBytes());
+        rfile.close();
+        int counter = 0;
+        menu.seek(0);
+        while (menu.getFilePointer() < menu.length() - 1) {
+            menu.readLine();
+            counter++;
+        }
+        menu.seek(menu.length());
+        String s = String.valueOf(counter);
+        String temp = s +"."+ name;
+       menu.writeBytes("\n");
+        menu.write(temp.getBytes());
     }
 }
